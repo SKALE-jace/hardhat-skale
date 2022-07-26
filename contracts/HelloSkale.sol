5cb4176271b41550250024ccf248de79e99affae
SKALE-jace <103141574+SKALE-jace@users.noreply.github.com>
2022-07-26 11:52:30 +0100

    Update HelloSkale.sol
    updated HelloSKALE return

--- contracts/HelloSkale.sol
+++ contracts/HelloSkale.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.9;
 
-contract HelloSkale {
+contract HelloSKALE {
     function sayHelloSkale() public pure returns (string memory) {
-        return "Hello Skale!";
+        return "Welcome to the ⵎＳＫＡＬＥＶＥＲＳＥ🚀";
     }
 }
