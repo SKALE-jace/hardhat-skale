be873cabcede0fadcefb779e3b94a9ba50e13d23
SKALE-jace <103141574+SKALE-jace@users.noreply.github.com>
2022-08-03 19:12:42 +0100

    Update HelloSkale.sol
    updated return statement

--- contracts/HelloSkale.sol
+++ contracts/HelloSkale.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.9;
 
 contract HelloSKALE {
     function sayHelloSkale() public pure returns (string memory) {
-        return "Welcome to the ⵎＳＫＡＬＥ🆅🅴🆁🆂🅴🚀";
+        return "Welcome to the SKALEverse";
     }
 }
